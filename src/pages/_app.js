84346c04cf36c81bdfaed309e385740db6fb4c0f
Ben Halverson <benhalverson33@gmail.com>
2021-05-07 21:26:49 -0700

    dark mode

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+
+import App from 'next/app'
+import withDarkMode, { MODE } from 'next-dark-mode'
+
+import '../styles/global.css'
+
+export default withDarkMode(App, { defaultMode: MODE.DARK })
---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import { useDarkMode } from 'next-dark-mode'
+import React, { Fragment } from 'react'
+
+export default function Home() {
+  const { autoModeActive, darkModeActive, switchToAutoMode, switchToDarkMode, switchToLightMode } = useDarkMode()
+
+  const findActive = (text) => {
+    if (autoModeActive) return text === 'auto'
+    else if (darkModeActive) return text === 'dark'
+    else return text === 'light'
+  }
+
+  const toggleMode = (text) => {
+    if (text === 'Auto') switchToAutoMode()
+    if (text === 'Dark') switchToDarkMode()
+    if (text === 'Light') switchToLightMode()
+  }
+
+  return (
+    <div className={`layout ${darkModeActive ? 'dark' : 'light'}`}>
+      <div className="switch">
+        {['Auto', 'Dark', 'Light'].map((text, index) => (
+          <Fragment key={index}>
+            <input
+              checked={findActive(text.toLowerCase())}
+              id={`_${index}`}
+              name="switch"
+              onChange={() => toggleMode(text)}
+              type="radio"
+            />
+            <label className="switch__label" htmlFor={`_${index}`}>
+              {text}
+            </label>
+          </Fragment>
+        ))}
+        <div className="switch__indicator" />
+      </div>
+    </div>
+  )
+}
